9d0ad7cefa107980a65e09cd6730a7105cda1874
Mark Wetzel <mark@markwetzel.com>
2013-03-10 16:37:14 -0800

    Add ListingFetcher class

---
+++ listingfetcher.rb
@@ -0,0 +1,18 @@
+class ListingFetcher
+  
+  def initialize
+  end
+
+  def self.fetch(uri)
+    doc = Nokogiri::HTML(open(uri))
+    listings = []
+    doc.css('html body blockquote p').each do |node|
+      listing = Listing.new(node.css('a').text)
+      listing.location = node.css('.itempn').text
+      listings << listing
+    end
+    # Delete the 'next 100 postings' entry.
+    listings.pop
+    listings
+  end
+end
--- main.rb
+++ main.rb
@@ -1,19 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 require_relative 'listing'
+require_relative 'listingfetcher'
 
-doc = Nokogiri::HTML(open("http://portland.craigslist.org/sof"))
-
-listings = []
-
-doc.css('html body blockquote p').each do |node|
-  listing = Listing.new(node.css('a').text)
-  listing.location = node.css('.itempn').text
-  listings << listing
-
-end
-
-# Delete the 'next 100 postings' entry.
-listings.pop
+listings = ListingFetcher.fetch("http://portland.craigslist.org/sof")
 
 listings.each  { |listing| puts listing }
